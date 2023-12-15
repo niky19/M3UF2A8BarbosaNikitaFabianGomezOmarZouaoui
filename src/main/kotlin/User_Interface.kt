50d8b53a1d4dfece5d6530de73e112829ec37bc5
Omar <omar.zouaoui.7e7@itb.cat>
2023-12-15 13:02:16 +0100

    MENU

--- src/main/kotlin/User_Interface.kt
+++ src/main/kotlin/User_Interface.kt
@@ -1,19 +1,52 @@
+import java.util.*
 
 fun main() {
     val anchoTablero = 12
     val altoTablero = 9
     val tablero = Board(altoTablero, anchoTablero)
 
-    tablero.showBoard()
-    var pieza = tablero.getRandomPiece()
-    colocarPiezaEnTablero(tablero, pieza, 5, 3)
+    println("¡Bienvenido a Tetris!")
 
+    val scanner = Scanner(System.`in`)
+
+    var continuarJuego = true
+
+    do {
+        println("Menú:")
+        println("1. Mostrar Tablero")
+        println("2. Colocar Pieza Aleatoria")
+        println("3. Salir")
+
+        print("Ingresa tu elección: ")
+        val opcion = scanner.nextLine()
+
+        when (opcion) {
+            "1" -> mostrarTablero(tablero)
+            "2" -> colocarPiezaAleatoria(tablero, scanner)
+            "3" -> {
+                println("Gràcies per jugar! Fins aviat!")
+                continuarJuego = false
+            }
+            else -> println("Opción no vàlida. Ingresa un número del 1 al 3")
+        }
+    } while (continuarJuego)
+}
+
+fun mostrarTablero(tablero: Board) {
+    println("Tauler actual:")
     tablero.showBoard()
 }
 
-fun colocarPiezaEnTablero(tablero: Board, pieza: Piece, x: Int, y: Int) {
-    pieza.positionX=x
-    pieza.positionY=y
+fun colocarPiezaAleatoria(tablero: Board, scanner: Scanner) {
+    val pieza = tablero.getRandomPiece()
+
+    print("Ingresa la posició X per la peça aleatoria: ")
+    val x = scanner.nextLine().toInt()
+
+    print("Ingresa la posició Y per la peça aleatoria: ")
+    val y = scanner.nextLine().toInt()
+
+    println("colocant peça a la posició ($x, $y)")
     tablero.placePiece(pieza)
+    tablero.showBoard()
 }
-
