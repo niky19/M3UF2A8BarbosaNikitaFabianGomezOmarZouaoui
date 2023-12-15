6637c9a3d7f2f69fab0d21004fadca0fe27405ff
Omar <omar.zouaoui.7e7@itb.cat>
2023-12-15 12:35:06 +0100

    movimiento de piezas

--- src/main/kotlin/User_Interface.kt
+++ src/main/kotlin/User_Interface.kt
@@ -1,38 +1,19 @@
-// en este archivo van las funciones relacionadas con la interfaz e interracción del usuario
-
-fun mostrarTablero(tablero: Array<Array<String>>) {
-    for (fila in tablero) {
-        for (casilla in fila) {
-            print("$casilla ")
-        }
-        println()
-    }
-}
 
 fun main() {
-    val anchoTablero = 10
-    val altoTablero = 20
-    val tablero = Array(altoTablero) { Array(anchoTablero) { "." } }
+    val anchoTablero = 12
+    val altoTablero = 9
+    val tablero = Board(altoTablero, anchoTablero)
 
+    tablero.showBoard()
+    var pieza = tablero.getRandomPiece()
+    colocarPiezaEnTablero(tablero, pieza, 5, 3)
 
-    val pieza = arrayOf(
-        arrayOf("X", "X"),
-        arrayOf("X", "X")
-    )
-
-
-    colocarPiezaEnTablero(tablero, pieza, 2, 3)
-
-
-    mostrarTablero(tablero)
+    tablero.showBoard()
 }
 
-fun colocarPiezaEnTablero(tablero: Array<Array<String>>, pieza: Array<Array<String>>, x: Int, y: Int) {
-    for (i in pieza.indices) {
-        for (j in pieza[i].indices) {
-            if (pieza[i][j] == "X") {
-                tablero[y + i][x + j] = "X"
-            }
-        }
-    }
+fun colocarPiezaEnTablero(tablero: Board, pieza: Piece, x: Int, y: Int) {
+    pieza.positionX=x
+    pieza.positionY=y
+    tablero.placePiece(pieza)
 }
+
