b7ebbba5fe1ab362080d1401a88a00e250c51b3c
niky <nikita.barbosa.7e5@itb.cat>
2023-12-15 13:02:40 +0100

    fix a placePiece y getPiecePosY

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -14,29 +14,28 @@ data class Board(var height: Int, var width: Int) {
     }
 
     fun setUpPieces(): List<Piece> {
-        var pieceO = Piece('O', listOf(listOf(1, 1), listOf(1, 1)), "red", 0, 5)
-        var pieceL = Piece('L', listOf(listOf(1, 0), listOf(1, 0), listOf(1, 1)), "blue", 0, 5)
-        var pieceJ = Piece('J', listOf(listOf(1, 0, 0), listOf(1, 1, 1)), "green", 0, 5)
-        var pieceI = Piece('I', listOf(listOf(1), listOf(1), listOf(1), listOf(1)), "yellow", 0, 5)
-        var piecesList = listOf(pieceO, pieceL, pieceJ, pieceI)
+        val pieceO = Piece('O', listOf(listOf(1, 1), listOf(1, 1)), "red", 0, 5)
+        val pieceL = Piece('L', listOf(listOf(1, 0), listOf(1, 0), listOf(1, 1)), "blue", 0, 5)
+        val pieceJ = Piece('J', listOf(listOf(1, 0, 0), listOf(1, 1, 1)), "green", 0, 5)
+        val pieceI = Piece('I', listOf(listOf(1), listOf(1), listOf(1), listOf(1)), "yellow", 0, 5)
+        val piecesList = listOf(pieceO, pieceL, pieceJ, pieceI)
         return piecesList
     }
 
     fun getRandomPiece(): Piece {
         val pieces = setUpPieces()
-        var random = Random.nextInt(0, pieces.size)
+        val random = Random.nextInt(0, pieces.size)
         return pieces[random]
     }
 
     //Cuando nos llegue la pieza la posicion X e Y seran las correctas
     fun placePiece(piece: Piece) {
-            for (i in 0..<piece.shape.size) {
-                for (j in 0..<piece.shape[i].size) {
-                    board[piece.positionY + i][piece.positionX + j] = piece.shape[i][j]
-                }
-            }
+    for (i in 0..<piece.shape.size) {
+        for (j in 0..<piece.shape[i].size) {
+            board[piece.positionY + i][piece.positionX + j] = piece.shape[i][j]
         }
-
+    }
+}
 
 
     fun moveRight(piece: Piece) {
@@ -47,22 +46,19 @@ data class Board(var height: Int, var width: Int) {
         piece.positionX -= 1
     }
 
-//Comprueba si se puede mover, comparando cada índice de la pieza con los índices adyacentes del tablero
-   fun getPiecePositionY(piece: Piece): Int {
-      var  myPositionY : Int
+    //Comprueba si se puede mover, comparando cada índice de la pieza con los índices adyacentes del tablero
+    fun getPiecePositionY(piece: Piece): Int {
         for (i in board.indices) {
-            for (j in 0..board[i].size) {
-                if(j in piece.positionX..piece.positionX+ piece.shape.lastIndex){
-                    myPositionY = j - piece.positionX
-                    if(board[i][myPositionY] == 1){
-                        return j+1
+            for (j in 0..<board[i].size) {
+                if (j in piece.positionX..piece.positionX + piece.shape[piece.shape.lastIndex].size) {
+                    if (board[i][j] == 1) {
+                        return board.size - j - piece.shape[piece.shape.lastIndex].size
                     }
                 }
             }
         }
-        return 0
+        return board.size - piece.shape[piece.shape.lastIndex].size
     }
-
 
 
     fun isLineComplete(): Boolean {
@@ -71,18 +67,20 @@ data class Board(var height: Int, var width: Int) {
     }
 
 }
-fun getPiecePositionY(piece: Piece) {
-    //TODO: hacer una funcion que calcule la altura de mi pieza (dado una X tienes que ver donde esta el limite que la puedes poner(la primera q choqe o el final del tablero)
-//flujo : mover derecha/izq haciendo la comprobacion y despues encontrar la posicion de la Y de la pieza, despues ponerla y revisar si hay q el;iminar alguna linea
-
-}
 
 
 fun main() {
-    val myBoard = Board(9, 12)
-    myBoard.showBoard()
-    var piece = Piece('O', listOf(listOf(1, 1), listOf(1, 1)), "red", 0, 5)
-    println(piece)
-    println(myBoard.getPiecePositionY(piece))
+    val board = Board(5, 5)
+    board.showBoard()
+    val piece = Piece('O', listOf(listOf(1, 1), listOf(1, 1)), "red", 0, 0)
+    println(piece.shape)
 
+    piece.positionY = board.getPiecePositionY(piece)
+
+    println("La pieza esta en la posicion ${piece.positionY}")
+
+    board.placePiece(piece)
+    board.showBoard()
+
+    println("La pieza esta en la posicion ${piece.positionY}")
 }
