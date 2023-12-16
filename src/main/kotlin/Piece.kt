276da698bcb1dbf74bb44a20a3916b26218aab54
niky <nikita.barbosa.7e5@itb.cat>
2023-12-16 19:05:36 +0100

    Board.kt:
    Agregar lista piecesOnBoard y mostrar piezas en colores
    Modificar placePiece para agregar pieza a piecesOnBoard
    Modificar removeCompletedLines para considerar piecesOnBoard
    
    Piece.kt:
    
    Modificar showPiece para imprimir colores
    
    Main.kt:
    
    Eliminar código comentado y funciones no utilizadas
    
    UserInterface.kt:
    
    Realizar cambios menores en los mensajes
    
    Utilidades.kt:
    
    Cambiar nombre de getPiecePositionY a findEmptyRowForPiece

--- src/main/kotlin/Piece.kt
+++ src/main/kotlin/Piece.kt
@@ -1,2 +1,32 @@
-class Piece(val name: Char, val shape : List<List<Int>>, val color : String, var positionY : Int, var positionX : Int)
+import java.util.Scanner
+import kotlin.random.Random
+
+class Piece(val name: Char, val shape: List<List<Int>>, val color: String, var positionY: Int, var positionX: Int) {
+
+    fun movePositionX(newPositionX: Int) {
+        positionX = newPositionX
+    }
+
+    fun showPiece() {
+        for (i in 0 until shape.size) {
+            for (j in 0 until shape[i].size) {
+                if (shape[i][j] == 1) {
+                    print(color)
+                }
+            }
+            println()
+        }
+        println()
+    }
+
+    fun getHorizontalSpace(): Int {
+        var maxHorizontalSpace = 0
+        for (i in 0 until shape.size) {
+            if (shape[i].sum() > maxHorizontalSpace) {
+                maxHorizontalSpace = shape[i].sum()
+            }
+        }
+        return maxHorizontalSpace
+    }
+}
 
--- src/main/kotlin/User_Interface.kt
+++ src/main/kotlin/User_Interface.kt
@@ -1,52 +1,90 @@
 import java.util.*
 
-fun main() {
-    val anchoTablero = 12
-    val altoTablero = 9
-    val tablero = Board(altoTablero, anchoTablero)
+//fun main() {
+//    val anchoTablero = 12
+//    val altoTablero = 9
+//    val tablero = Board(altoTablero, anchoTablero)
+//
+//    println("¡Bienvenido a Tetris!")
+//
+//    val scanner = Scanner(System.`in`)
+//
+//    var continuarJuego = true
+//
+//    do {
+//        println("Menú:")
+//        println("1. Mostrar Tablero")
+//        println("2. Colocar Pieza Aleatoria")
+//        println("3. Salir")
+//
+//        print("Ingresa tu elección: ")
+//        val opcion = scanner.nextLine()
+//
+//        when (opcion) {
+//            "1" -> mostrarTablero(tablero)
+//            "2" -> colocarPiezaAleatoria(tablero, scanner)
+//            "3" -> {
+//                println("Gràcies per jugar! Fins aviat!")
+//                continuarJuego = false
+//            }
+//            else -> println("Opción no vàlida. Ingresa un número del 1 al 3")
+//        }
+//    } while (continuarJuego)
+//}
 
-    println("¡Bienvenido a Tetris!")
+import java.util.*
 
-    val scanner = Scanner(System.`in`)
+class User_Interface() {
+    val sc = Scanner(System.`in`)
 
-    var continuarJuego = true
+    fun startGame() {
+        val board = createBoard()
+        initGame(board)
+    }
 
-    do {
+    private fun initGame(board: Board) {
+        println("El juego ha empezado!")
+        while (!board.isGameOver) {
+            showMenu()
+            val userNextAction = getIntInRange(1, 3, sc)
+            when (userNextAction) {
+                1 -> board.showBoard()
+                2 -> placeNextPiece(board)
+                3 -> board.isGameOver = true
+            }
+            board.removeCompletedLines()
+            board.showBoard()
+        }
+        println("Gràcies per jugar! Fins aviat!")
+    }
+
+    private fun createBoard(): Board {
+        println("¡Bienvenido a Tetris!")
+        println("Para comenzar elija el ancho del tablero")
+        val width = checkInt(sc)
+        println("Ahora el alto del tablero")
+        val height = checkInt(sc)
+        println("Tu tablero ha sido configurado correctamente")
+        val board = Board(height, width)
+        board.showBoard()
+        return board
+    }
+
+    private fun showMenu() {
         println("Menú:")
         println("1. Mostrar Tablero")
         println("2. Colocar Pieza Aleatoria")
         println("3. Salir")
+    }
 
-        print("Ingresa tu elección: ")
-        val opcion = scanner.nextLine()
-
-        when (opcion) {
-            "1" -> mostrarTablero(tablero)
-            "2" -> colocarPiezaAleatoria(tablero, scanner)
-            "3" -> {
-                println("Gràcies per jugar! Fins aviat!")
-                continuarJuego = false
-            }
-            else -> println("Opción no vàlida. Ingresa un número del 1 al 3")
-        }
-    } while (continuarJuego)
+    private fun placeNextPiece(board: Board) {
+        val piece = getRandomPiece()
+        piece.showPiece()
+        println("Quant vols moure la peça en horitzontal cap a la dreta?")
+        val maxPosition = board.width - piece.getHorizontalSpace()
+        val newPositionX = getIntInRange(0, maxPosition, sc)
+        piece.movePositionX(newPositionX)
+        board.placePiece(piece)
+    }
 }
 
-fun mostrarTablero(tablero: Board) {
-    println("Tauler actual:")
-    tablero.showBoard()
-}
-
-fun colocarPiezaAleatoria(tablero: Board, scanner: Scanner) {
-    val pieza = tablero.getRandomPiece()
-
-    print("Ingresa la posició X per la peça aleatoria: ")
-    val x = scanner.nextLine().toInt()
-
-    print("Ingresa la posició Y per la peça aleatoria: ")
-    val y = scanner.nextLine().toInt()
-
-    println("colocant peça a la posició ($x, $y)")
-    tablero.placePiece(pieza)
-    tablero.showBoard()
-}
