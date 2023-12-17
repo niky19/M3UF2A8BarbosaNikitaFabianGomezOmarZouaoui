b5c48854bc442f9058d1d1bc6393d9ca4bb7adc5
niky <nikita.barbosa.7e5@itb.cat>
2023-12-17 23:11:36 +0100

    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
 }
 
